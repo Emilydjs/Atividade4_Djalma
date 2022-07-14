ff373ba4ea12f3de7d0e249684805b3bc3dd0531
User <User@DESKTOP-CU12B7G>
2022-07-13 22:51:42 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade4_Djalma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4_Djalma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Contador-----");
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.WriteLine("-----Fim de Contagem-----");
+            Console.ReadKey();
+        }
+    }
+}
